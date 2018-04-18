d9cad0c50552957f7e4971b13c0c8578abcd2270
Rachada Chairangsaris <rachada_32@hotmail.com>
2018-04-18 13:56:10 -0400

    removed dropdown menu, added new responsive nav bar

--- js/app.js
+++ js/app.js
@@ -1,37 +1,17 @@
+var menu = document.getElementById("menu");
+var navTop = document.getElementById("navTopResponsive");
 
+function show() {
 
+	menu.style.display = "flex";
+	navTop.style.display = "none";		
+}
 
-// function active() {
+function hide() {
 
-// 	var menu = document.getElementById("menu");
-
-
-// 		if (menu.style.display == 'none') {
-// 			menu.style.display = "block";
-// 		} else {
-// 			menu.style.display = "none";
-// 		}
-
-// 	var screenWidth = screen.availWidth;	
-// 	console.log(screenWidth);
-// 	if (screenWidth < 800) {
-// 	var item1 = document.getElementById("item1").offsetHeight;
-// 	var item2 = document.getElementById("item2").offsetHeight;
-// 	var item3 = document.getElementById("item3").offsetHeight;
-// 	var item4 = document.getElementById("item4").offsetHeight;
-// 	var item5 = document.getElementById("item5").offsetHeight;
-
-// 	var dropDownMenuHeight = (item1 + item2 + item3 + item4 + item5)
-
-// 	var headerStart = document.getElementById("header");
-
-// 	headerStart.style.marginTop = dropDownMenuHeight+"px";
-// 	} else {
-// 		headerStart.style.marginTop = "0px";
-// 	}
-// }
-
-
+	menu.style.display = "none";
+	navTop.style.display = "flex";		
+}
 
 
 
